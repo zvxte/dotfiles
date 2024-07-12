918c7b55110c1c28dcca1cc6341710e45c8d8a04
zvxte <zvxte.codes@gmail.com>
2024-07-12 18:40:26 +0200

    update

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -1123,6 +1123,10 @@ user_pref("extensions.pocket.enabled", false);
 user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
 
 user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("gfx.webrender.all", true);
+user_pref("media.av1.enabled", false);
+user_pref("gfx.x11-egl.force-enabled", true);
+user_pref("widget.dmabuf.force-enabled", true);
 
 
 /* BETTERFOX SETTINGS FOR SMOOTH SCROLL */
