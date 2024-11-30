36b73c89f892705c530651d10c4554d6de727289
zvxte <zvxte.codes@gmail.com>
2024-11-30 21:54:26 +0100

    hide accessibility from right click menu

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -19,6 +19,7 @@ user_pref("full-screen-api.warning.delay", 0);
 user_pref("full-screen-api.warning.timeout", 0);
 user_pref("full-screen-api.transition-duration.enter", "0 0");
 user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("devtools.accessibility.enabled", false);
 
 // UX
 user_pref("middlemouse.contentLoadURL", false);
